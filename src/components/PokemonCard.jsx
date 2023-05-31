1b0962cc15432ff97ac436029f912b852b6bc7dc
Killian Cousseau <killian.cousseau@gmail.com>
2023-05-31 10:34:49 +0200

    React Basics 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,31 @@
 function PokemonCard() {
-  return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
-  );
+  const pokemon = pokemonList[0];
+  if (pokemon.imgSrc === undefined) {
+    return (
+      <figure>
+        <p>???</p>
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  } else {
+    return (
+      <figure>
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  }
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
